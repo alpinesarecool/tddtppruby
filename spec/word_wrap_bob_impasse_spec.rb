7489c902a0d5dea619dde3feddf2cdcb9db42e24
nitinrajkumarparuchuri <nitinrajkumar502@gmail.com>
2025-08-08 18:26:37 +0000

    statement->statements spec that lets one write a transformation but there is no better transformation than expression->function

--- spec/word_wrap_bob_impasse_spec.rb
+++ spec/word_wrap_bob_impasse_spec.rb
@@ -25,5 +25,6 @@ RSpec.describe "#word_wrap_bob_impasse" do
   end
   it "breaks the word at length when longer than counter" do
     expect(word_wrap_bob_impasse("helloworld",5)).to eq("hello\nworld")
+    expect(word_wrap_bob_impasse("helloworld",6)).to eq("hellow\norld")
   end
 end
--- word_wrap_bob_impasse.rb
+++ word_wrap_bob_impasse.rb
@@ -3,6 +3,9 @@ def word_wrap_bob_impasse(text, counter)
   if text.nil? || text.empty?
     return ""
   else
+    #went over the board missed to write the constant in the previous step
+    # which gives us the opportunity to write one more spec to transform it expression->function by adding a statement->statements spec
+    #"hello"+"\n"+"world"
     text.slice(0...counter)+"\n"+text.slice(counter..)
   end
 end
