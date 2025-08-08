47a3b1111bfffad88dacd4422b5c4084f14cebf7
nitinrajkumarparuchuri <nitinrajkumar502@gmail.com>
2025-08-08 12:46:11 +0000

    another conditional that would drive multiple words, should have written this first but the apprehenshion was caused by use of each_slice which is an enumerator which translates to a loop but a test case should be minimal and TPP guides in that direction

--- spec/word_wrap_spec.rb
+++ spec/word_wrap_spec.rb
@@ -17,4 +17,8 @@ RSpec.describe "#word_wrap" do
   it "returns by breaking the word into new line when the text has multiple words and each word's length is less than the counter" do
     expect(word_wrap("Hello world", 6)).to eq("Hello\nworld")
   end
+
+  it "returns by breaking the word into new line when the text has single word word's length is less than the counter" do
+    expect(word_wrap("Helloworld", 6)).to eq("Hellow\norld")
+  end
 end
--- word_wrap.rb
+++ word_wrap.rb
@@ -1,5 +1,8 @@
 def word_wrap(word, counter)
   return word if counter==0
-  
+  if(word.length>counter && word.split(" ").count ==1)
+     return word.chars.each_slice(counter).map(&:join).join("\n")
+  end
+
   word.split(" ").join("\n")
 end
