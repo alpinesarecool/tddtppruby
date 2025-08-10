dcd7647c2b6d3a7a8890786b243481b0d6c3ead1
nitinrajkumarparuchuri <nitinrajkumar502@gmail.com>
2025-08-10 16:57:17 +0000

    refactor

--- ceaser_cipher.rb
+++ ceaser_cipher.rb
@@ -1,12 +1,8 @@
 def ceaser_cipher(message, key)
   return message if key==0 || message==""
-  result=""
-  message.chars.each do |char|
-    if (char.ord+key)>122
-      result +=(char.ord+key-26).chr
-    else
-      result +=(char.ord+key).chr
-    end
-  end
-  result
+
+  message.chars.map do |char|
+    shift_in_ascii = char.ord+key
+    (shift_in_ascii)>122 ? (shift_in_ascii-26).chr : (shift_in_ascii).chr
+  end.join
 end
